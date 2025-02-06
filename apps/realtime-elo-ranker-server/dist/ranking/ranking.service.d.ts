671f733922d8580943a875b5a140fa56f4bbd0e9
ChabilanLeni <chabilan.leni@gmail.com>
2025-02-06 17:35:24 +0100

    ajout de tout pour faire fonctionner l'event emmiter avec les match & fix du draw

--- apps/realtime-elo-ranker-server/dist/ranking/ranking.service.d.ts
+++ apps/realtime-elo-ranker-server/dist/ranking/ranking.service.d.ts
@@ -1,9 +1,11 @@
 import { PlayerService, Player } from '../player/player.service';
 import { MatchResult } from '../match/match.service';
+import { EventEmitterService } from '../event-emitter/event-emitter-service';
 export declare class RankingService {
     private readonly playerService;
+    private readonly eventEmitter;
     private readonly K;
-    constructor(playerService: PlayerService);
+    constructor(playerService: PlayerService, eventEmitter: EventEmitterService);
     private rankingUpdates;
     getRanking(): Player[];
     getRankingUpdates(): import("rxjs").Observable<Player[]>;
